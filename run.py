cccc40ec2fb34ede3e61d11c831572b168a3572a
daun.jeong <daun.jeong@navercorp.com>
2022-12-08 01:04:32 +0900

    3

--- run.py
+++ run.py
@@ -1,16 +1,12 @@
 import random
 from typing import List
+import math
 from argparse import ArgumentParser
 
 
-def split_group(samples: List, size: int):
-    p = len(samples)
-    for _ in range(0, len(samples), size):
-        for _ in range(size):
-            i = random.randrange(p)
-            p -= 1
-            samples[i], samples[p] = samples[p], samples[i]
-        yield samples[p:p + size]
+def split_group(samples: List, size: int) -> List[List]:
+    random.shuffle(samples)
+    return [samples[i::size] for i in range(size)]
 
 
 if __name__ == '__main__':
@@ -20,16 +16,19 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     comment_body = open('comment-body.md', 'w')
-
-    comment_body.write("Members\n")
     members = [
        "김수영", "김원호", "김유리",
        "류나현", "신규용", "이다운",
        "임승재", "정다운", "정승재",
-       "정지수", 'Max', "Xingdong"
+       "정지수", 'Max',# "Xingdong"
     ]
-    team_size = 4
-    for group_idx, group in enumerate(split_group(members, team_size)):
-        comment_body.write(f'{group_idx:02} | {group}\n')
+    group_num = 3
+    team_size = math.ceil(len(members) / group_num)
+    comment_body.write('|Team|' + '|'.join([str(i) for i in range(team_size)]) + '|\n')
+    comment_body.write('|' + '---|' * (team_size + 1) + '\n')
+    for group_idx, group in enumerate(split_group(members, group_num)):
+        group = group + [' '] * (team_size - len(group))
+        group_str = '|'.join(group)
+        comment_body.write(f'|{group_idx:02}|{group_str}|\n')
 
     comment_body.close()
