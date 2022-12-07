2075cb61a688f5c58652c942ef9f8026e092bf31
daun.jeong <daun.jeong@navercorp.com>
2022-12-08 01:11:26 +0900

    3

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
+import os
 import random
-from typing import List
 import math
 from argparse import ArgumentParser
-
+from typing import List
+import sys
 
 def split_group(samples: List, size: int) -> List[List]:
     random.shuffle(samples)
@@ -10,25 +11,36 @@ def split_group(samples: List, size: int) -> List[List]:
 
 
 if __name__ == '__main__':
+    print(os.environ['github.event.comment.body'])
+
     parser = ArgumentParser()
+    parser.add_argument('-s', '--seed', type=int, default=None)
     parser.add_argument('-m', '--members', nargs='*')
-    parser.add_argument('-t', '--team-size', type=int, default=4)
+    parser.add_argument('-n', '--group_num', type=int, default=3)
     args = parser.parse_args()
 
-    comment_body = open('comment-body.md', 'w')
-    members = [
-       "김수영", "김원호", "김유리",
-       "류나현", "신규용", "이다운",
-       "임승재", "정다운", "정승재",
-       "정지수", 'Max',# "Xingdong"
-    ]
-    group_num = 3
+    if args.seed is not None:
+        random.seed(args.seed)
+
+    if args.members is not None:
+        members = [
+           "김수영", "김원호", "김유리",
+           "류나현", "신규용", "이다운",
+           "임승재", "정다운", "정승재",
+           "정지수", 'Max', "Xingdong",
+        ]
+    else:
+        members = args.members
+
+    group_num = args.group_num
     team_size = math.ceil(len(members) / group_num)
-    comment_body.write('|Team|' + '|'.join([str(i) for i in range(team_size)]) + '|\n')
+
+    comment_body = open('comment-body.md', 'w')
+    comment_body.write('|Team|' + '|'.join([' ' for i in range(team_size)]) + '|\n')
     comment_body.write('|' + '---|' * (team_size + 1) + '\n')
     for group_idx, group in enumerate(split_group(members, group_num)):
         group = group + [' '] * (team_size - len(group))
         group_str = '|'.join(group)
-        comment_body.write(f'|{group_idx:02}|{group_str}|\n')
+        comment_body.write(f'|{group_idx}|{group_str}|\n')
 
     comment_body.close()
